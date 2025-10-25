65f888ce2338918e8fb3ba72d18272f9d6096fc7
maaztausif <maaztausif48@gmail.com>
2025-10-25 02:19:21 +0200

    Project Created

---
+++ src/main/java/com/maaz/Java_ToDoApp/JavaToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.Java_ToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaz/Java_ToDoApp/JavaToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaz.Java_ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
